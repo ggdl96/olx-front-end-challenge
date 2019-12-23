1b99afd06c4a943f440c149d1446ba38bc654e2e
Gustavo Gabriel De Luca <gustavogdeluca96@gmail.com>
2019-12-23 07:31:28 -0300

    Update article component

--- src/common/Article/index.js
+++ src/common/Article/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
+import { $grayLightColor } from '../../styles/constants'
 
 const ArticleContent = styled.div`
     padding-left: 6px;
 `
+
+const ArticleHeader = styled.header`
+    border-bottom: 1px ${$grayLightColor} solid;
+`
 function Article({ header, content }) {
     return (
         <article>
-            <header>
+            <ArticleHeader>
                 <h3>{header}</h3>
-                <ArticleContent>{content}</ArticleContent>
-            </header>
+            </ArticleHeader>
+            <ArticleContent>{content}</ArticleContent>
         </article>
     )
 }
